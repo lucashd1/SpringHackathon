363e53ee376addb96bc539be71a476ce908a02b8
Vanessa Forney <vanessaforney@gmail.com>
2015-04-18 16:39:00 -0700

    SmartLoans page done!!

--- SpringHackathon/SpringHackathon/EstimatedScoreViewController.swift
+++ SpringHackathon/SpringHackathon/EstimatedScoreViewController.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
-class EstimateScoreViewController: UIViewController {
-    @IBOutlet weak var estimatedScoreLabel: UILabel!
+class EstimateScoreViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var score: UIWebView!
+    @IBOutlet weak var smartLoanDescription: UILabel!
     
     var loan: Loan!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        estimatedScoreLabel.text = "\(self.loan.estimated)"
+        self.score.delegate = self
+        
+        if let path = NSBundle.mainBundle().pathForResource("index", ofType: "html") {
+            let urlPath = NSURL.fileURLWithPath(path)
+            if let contents = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil) {
+                self.score.loadHTMLString(contents, baseURL: urlPath)
+            }
+        }
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.score.stringByEvaluatingJavaScriptFromString("showData(\(self.loan.estimated))")
     }
 }
