992d6ea6cca226a35e04e4107c0156c238025eeb
Max <max@Jessies-MacBook-Pro.local>
2015-04-18 02:16:01 -0700

    Add segments for loan estimates.

--- SpringHackathon/SpringHackathon/LoanEstimatorViewController.swift
+++ SpringHackathon/SpringHackathon/LoanEstimatorViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class LoanEstimatorViewController: UIViewController {
+    enum Loan {
+        case Auto, Mortgage
+    }
+    
     @IBOutlet weak var selector: UISegmentedControl!
     @IBOutlet weak var minLoan: UILabel!
     @IBOutlet weak var maxLoan: UILabel!
@@ -18,13 +22,40 @@ class LoanEstimatorViewController: UIViewController {
     @IBOutlet weak var monthlyPayment: UILabel!
     @IBOutlet weak var estimateScoreButton: UIButton!
     
+    @IBAction func selectorChanged(sender: AnyObject) {
+        switch selector.selectedSegmentIndex {
+        case 0:
+            loanTerm.removeAllSegments()
+            loanTerm.insertSegmentWithTitle("15", atIndex: 0, animated: true)
+            loanTerm.insertSegmentWithTitle("30", atIndex: 1, animated: true)
+            loanTermUnit.text = "years"
+            selected = Loan.Mortgage
+            
+        case 1:
+            loanTerm.removeAllSegments()
+            loanTerm.insertSegmentWithTitle("36", atIndex: 0, animated: true)
+            loanTerm.insertSegmentWithTitle("48", atIndex: 1, animated: true)
+            loanTerm.insertSegmentWithTitle("60", atIndex: 2, animated: true)
+            loanTermUnit.text = "months"
+            selected = Loan.Auto
+            
+        default:
+            println("test")
+        }
+    }
+    
     var user: User?
+    var selected = Loan.Auto
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let tabBarController = self.tabBarController as? TabBarController {
             self.user = tabBarController.user
+        }
+        
+        if let font = UIFont(name: "IntroSemiBoldCaps", size: 14) {
+            estimateScoreButton.titleLabel!.font =  font
         }
     }
     
@@ -35,7 +66,4 @@ class LoanEstimatorViewController: UIViewController {
             }
         }
     }
-    
-    // Home 30 year fixed, 15 year fixed
-    // Auto 36 month new, 48 month new, 60 month new
 }
